fcb48d2d55acc62322e7c4c2112ccb46f7ad6caa
Marco Edoardo Duma <marcoedoardo.duma@studio.unibo.it>
2021-12-14 16:22:37 +0100

    added TexField/Email/Password state with validation

---
+++ app/src/main/java/unibo/it/sk8/auth/PasswordState.kt
@@ -0,0 +1,32 @@
+package unibo.it.sk8.auth
+
+class PasswordState :
+    TextFieldState(validator = ::isPasswordValid, errorFor = ::passwordValidationError)
+
+class ConfirmPasswordState(private val passwordState: PasswordState) : TextFieldState() {
+    override val isValid: Boolean
+        get() = passwordAndConfirmationValid(passwordState.text, text)
+
+    override fun getError(): String? {
+        return if (showErrors()) {
+            passwordConfirmationError()
+        } else null
+    }
+}
+
+private fun passwordAndConfirmationValid(password: String, confirmedPassword: String): Boolean {
+    return isPasswordValid(password)
+}
+
+private fun isPasswordValid(password: String): Boolean {
+    return password.trim().length > 3
+}
+
+@Suppress("UNUSED_PARAMETER")
+private fun passwordValidationError(password: String): String {
+    return "Invalid password"
+}
+
+private fun passwordConfirmationError(): String {
+    return "Passwords don't match"
+}
