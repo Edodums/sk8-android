6a1b5c33202affe368416943da246ba4639097d7
Marco Edoardo Duma <marcoedoardo.duma@studio.unibo.it>
2022-01-11 11:31:18 +0100

    added core and test libraries

---
+++ libraries/core/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id(GradlePlugin.ANDROID_LIBRARY)
+}
+
+dependencies {
+    implementation(Deps.android.ktx)
+    implementation(Deps.android.material)
+    implementation(Deps.coroutines.android)
+    implementation(Deps.coroutines.core)
+    implementation(Deps.koin.android)
+}
---
+++ libraries/core/src/main/java/unibo/it/core/di/CoreModule.kt
@@ -0,0 +1,5 @@
+package unibo.it.core.di
+
+import org.koin.dsl.module
+
+val coreModule = module {  }
