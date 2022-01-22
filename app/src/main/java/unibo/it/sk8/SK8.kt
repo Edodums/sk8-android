845ae06a7bb5ce2a615ae9eab366e932bbb8c474
Marco Edoardo Duma <marcoedoardo.duma@studio.unibo.it>
2022-01-22 20:02:27 +0100

    added modules to startKoin, fixed android logger due to a koin issue

--- app/src/main/java/unibo/it/sk8/SK8.kt
+++ app/src/main/java/unibo/it/sk8/SK8.kt
@@ -3,15 +3,21 @@ package unibo.it.sk8
 import android.app.Application
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.InternalCoroutinesApi
+import org.koin.android.BuildConfig
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 import unibo.it.auth.di.authModule
+import unibo.it.controls.di.controlsModule
 import unibo.it.datastore.di.dataStoreModule
 import unibo.it.domain.di.domainModule
 import unibo.it.loading.di.loadingModule
+import unibo.it.lookup.di.lookupModule
+import unibo.it.menu.di.menuModule
 import unibo.it.repository.di.repositoryModule
 import unibo.it.sk8.di.appModule
+
 
 @InternalCoroutinesApi
 @ExperimentalCoroutinesApi
@@ -23,12 +29,15 @@ class SK8 : Application() {
 
     private fun initKoin() {
         startKoin {
-            androidLogger()
+            androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
             androidContext(this@SK8)
             modules(
                 appModule +
                     loadingModule +
                     authModule +
+                    menuModule +
+                    lookupModule +
+                    controlsModule +
                     domainModule +
                     repositoryModule +
                     dataStoreModule
